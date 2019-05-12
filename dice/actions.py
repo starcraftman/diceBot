2fddca65372bd424db9782b59e530e10e4b79d8d
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2019-05-12 13:38:56 -0400

    FIX: Lower strings not lists, typo.

--- dice/actions.py
+++ dice/actions.py
@@ -512,7 +512,7 @@ class Songs(Action):
             msg = msg.replace(self.bot.prefix + 'songs -a', '')
             parts = re.split(r'\s*,\s*', msg)
             parts = [part.strip() for part in parts]
-            name, url, tags = parts[0].lower(), parts[1], [x for x in parts[2:].lower()]
+            name, url, tags = parts[0].lower(), parts[1], [x.lower() for x in parts[2:]]
             song = self.add(name, url, tags)
 
             reply = '__Song Added__\n\n' + format_a_song(1, song)
