e8ab205343b73fb9f8ded2bf6b1caa9556113bce
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2022-11-26 23:05:54 -0500

    Allow cert or pass authentication to atlas.

--- dicedb/__init__.py
+++ dicedb/__init__.py
@@ -6,9 +6,17 @@ Followed simple tutorial for tutorial on motor.
 
 See: https://motor.readthedocs.io/en/stable/tutorial-asyncio.html
 """
+import os
+import pathlib
+
+import certifi
 import motor.motor_asyncio
 
 LOCAL_URL = 'mongodb://localhost:31000'
+ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+CERT_URL = pathlib.Path(os.path.join(ROOT, 'atlasCert.url.private'))
+CERT_PAT = pathlib.Path(os.path.join(ROOT, 'atlasCert.private'))
+PASS_PAT = pathlib.Path(os.path.join(ROOT, 'atlasPass.private'))
 
 
 def get_db_client(with_database='dice'):
@@ -16,11 +24,25 @@ def get_db_client(with_database='dice'):
     Returns the db client to mongo, after selecting the database first.
     If you want raw client, pass None.
 
-    Store the access line locally in atlas.private in root of project.
+    When X509 cert found at CERT_PAT, authenticate via X509.
+    CERT_URL will hold the url to authenticate against with cert.
+    Otherwise fallback to authentication via URL user/pass login found in PASS_PAT
+
+    Args:
+        with_database: Return this collection from top level client. Default dice
     """
-    with open('atlas.private', encoding='utf-8') as fin:
-        atlas_url = fin.read().strip()
-        client = motor.motor_asyncio.AsyncIOMotorClient(atlas_url)
+    if CERT_PAT.exists():
+        with open(CERT_URL, encoding='utf-8') as fin:
+            client = motor.motor_asyncio.AsyncIOMotorClient(
+                fin.read().strip(),
+                authMechanism="MONGODB-X509",
+                tls=True,
+                tlsCertificateKeyFile=str(CERT_PAT),
+                tlsCAFile=certifi.where(),
+            )
+    else:
+        with open(PASS_PAT, encoding='utf-8') as fin:
+            client = motor.motor_asyncio.AsyncIOMotorClient(fin.read().strip())
 
     if with_database:
         client = client[with_database]
